3193be1016672911e9e6641c52b984b0b2d93ee6
sujin <ikosdu60@gmail.com>
2022-12-12 22:08:28 +0900

    [todo] feat: 로그인 여부에 따른 리다이렉트 처리  #3

--- src/ui/pages/TodoList.tsx
+++ src/ui/pages/TodoList.tsx
@@ -1,11 +1,16 @@
+import { useEffect } from 'react'
 import { useNavigate } from 'react-router-dom'
 
 const TodoList = () => {
-  const navigater = useNavigate()
+  const navigate = useNavigate()
   const onClickHandler = () => {
     localStorage.removeItem('token')
-    navigater('/')
+    navigate('/')
   }
+
+  useEffect(() => {
+    if (!localStorage.getItem('token')) navigate('/')
+  }, [])
   return <button onClick={onClickHandler}>로그아웃</button>
 }
 
